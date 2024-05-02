e2b80a938162005effd088d34225e4d06d95d315
jessai <maya.jessai@gmail.com>
2024-05-02 11:55:06 -0600

    wworkspaces setup

---
+++ hello/src/main.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Window Conf".to_owned(),
+        fullscreen: true,
+        platform: miniquad::conf::Platform {
+            linux_backend: miniquad::conf::LinuxBackend::X11Only,
+            ..Default::default()
+        },
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        next_frame().await
+    }
+}

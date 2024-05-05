36a4b6790167b4a6b668ebcb931d4e4ecb1b09ff
jessai <maya.jessai@gmail.com>
2024-05-05 01:07:59 -0600

    feature/p5 rust bindings

--- color-palette/src/lib.rs
+++ color-palette/src/lib.rs
@@ -1,18 +1,17 @@
-use macroquad::color::Color;
 use rand::Rng;
 
-pub const BLACK: Color = Color::new(0.0, 0.07, 0.09, 1.0);
-pub const DARK_BLUE: Color = Color::new(0.0, 0.37, 0.45, 1.0);
-pub const BLUE: Color = Color::new(0.039, 0.576, 0.588, 1.0);
-pub const LIGHT_BLUE: Color = Color::new(0.580, 0.824, 0.741, 1.0);
-pub const BEIGE: Color = Color::new(0.914, 0.847, 0.65, 1.0);
-pub const YELLOW: Color = Color::new(0.933, 0.607, 0.0, 1.0);
-pub const ORANGE: Color = Color::new(0.792, 0.404, 0.007, 1.0);
-pub const DARK_ORANGE: Color = Color::new(0.733, 0.243, 0.012, 1.0);
-pub const RED: Color = Color::new(0.682, 0.125, 0.07, 1.0);
-pub const DARK_RED: Color = Color::new(0.608, 0.133, 0.149, 1.0);
+pub const BLACK: (f32, f32, f32) = (0.0, 0.07, 0.09);
+pub const DARK_BLUE: (f32, f32, f32) = (0.0, 0.37, 0.45);
+pub const BLUE: (f32, f32, f32) = (0.039, 0.576, 0.588);
+pub const LIGHT_BLUE: (f32, f32, f32) = (0.580, 0.824, 0.741);
+pub const BEIGE: (f32, f32, f32) = (0.914, 0.847, 0.65);
+pub const YELLOW: (f32, f32, f32) = (0.933, 0.607, 0.0);
+pub const ORANGE: (f32, f32, f32) = (0.792, 0.404, 0.007);
+pub const DARK_ORANGE: (f32, f32, f32) = (0.733, 0.243, 0.012);
+pub const RED: (f32, f32, f32) = (0.682, 0.125, 0.07);
+pub const DARK_RED: (f32, f32, f32) = (0.608, 0.133, 0.149);
 
-pub const PALETTE: [Color;10] = [
+pub const PALETTE: [(f32, f32, f32); 10] = [
     BLACK,
     DARK_BLUE,
     BLUE,
@@ -25,7 +24,7 @@ pub const PALETTE: [Color;10] = [
     DARK_RED,
 ];
 
-pub fn random_color() -> Color {
+pub fn random_color() -> (f32, f32, f32) {
     let mut rng = rand::thread_rng();
     let ind = rng.gen_range(0..PALETTE.len());
     return PALETTE[ind];
@@ -38,6 +37,9 @@ mod tests {
     #[test]
     fn random_color() {
         let result = super::random_color();
-        assert!(PALETTE.contains(&result), "Returned color is not in the palette");
+        assert!(
+            PALETTE.contains(&result),
+            "Returned color is not in the palette"
+        );
     }
 }

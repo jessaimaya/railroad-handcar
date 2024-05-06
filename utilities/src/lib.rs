e8f71ff841cd366f122ca17acc0463928b35e504
jessai <maya.jessai@gmail.com>
2024-05-05 23:33:48 -0600

    fix/map num range

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -1,21 +1,6 @@
 /// Re-maps a number from one range to another.
-pub fn map(
-    n: f32,
-    initial_1: f32,
-    final_1: f32,
-    initial_2: f32,
-    final_2: f32,
-    delimited: Option<bool>,
-) -> f32 {
-    let res = (n - initial_1) / (final_1 - initial_1) * (final_2 - initial_2) + initial_2;
-    if delimited.is_none() || delimited.is_some() == false {
-        return res;
-    }
-    if initial_2 < final_2 {
-        constrain(res, initial_2, final_2)
-    } else {
-        constrain(res, final_2, initial_2)
-    }
+pub fn map_num(num: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) -> f32 {
+    ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
 
 /// Constrains a number between a minimum and maximum value.
@@ -38,15 +23,9 @@ mod tests {
     }
     #[test]
     fn test_map() {
-        let top = super::map(10.0, 0.0, 10.0, 0.0, 1.0, None);
+        let top = super::map_num(10.0, 0.0, 10.0, 0.0, 1.0);
         assert_eq!(top, 1.0);
-        let bot = super::map(1.0, 1.0, 10.0, 0.0, 1.0, None);
+        let bot = super::map_num(1.0, 1.0, 10.0, 0.0, 1.0);
         assert_eq!(bot, 0.0);
-        let top_limit = super::map(200.0, 0.0, 100.0, 0.0, 1.0, Some(true));
-        assert_eq!(top_limit, 1.0);
-        let top_no_limit = super::map(200.0, 0.0, 100.0, 0.0, 1.0, Some(false));
-        assert_eq!(top_no_limit, 1.0);
-        let bot_no_limit = super::map(-1.0, 0.0, 100.0, 1.0, 0.0, Some(true));
-        assert_eq!(bot_no_limit, 1.0);
     }
 }
